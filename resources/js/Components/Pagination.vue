d0ab7899f57b60b4c0e57aee364d6cf161c205fd
CyrilToxage <mcyril2106@gmail.com>
2024-12-17 02:03:16 +0100

    correction pagination

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,29 +1,16 @@
 <template>
-    <nav v-if="links.length > 3" class="flex items-center justify-center mt-4 space-x-2">
-        <button v-for="(link, index) in links" :key="index" :class="[
-            'px-3 py-1 border rounded',
-            link.active ? 'bg-blue-500 text-white border-blue-500' : 'bg-white text-blue-500 border-gray-300',
-            link.url ? 'cursor-pointer' : 'cursor-not-allowed',
-        ]" :disabled="!link.url" @click.prevent="goTo(link.url)">
-            <span v-html="link.label"></span>
-        </button>
-    </nav>
+    <div class="flex justify-center mt-4">
+        <ul class="inline-flex -space-x-px">
+            <li v-for="link in links" :key="link.label">
+                <a :href="link.url" v-html="link.label" class="px-4 py-2 border rounded"
+                    :class="{ 'bg-gray-200': link.active }"></a>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script setup>
 defineProps({
     links: Array,
 });
-
-const goTo = (url) => {
-    if (url) {
-        window.location.href = url;
-    }
-};
 </script>
-
-<style scoped>
-button:disabled {
-    opacity: 0.5;
-}
-</style>
